656fb87a18f9282f90e70b5ba51f0864cbfd0e2c
suha <gusuhawoo@gmail.com>
2024-01-24 21:28:44 +0900

    Add : input & button

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
+import AddTodo from '../AddTodo/AddTodo';
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
     { id: '1', text: 'Hello', status: 'active' },
     { id: '2', text: 'study', status: 'active' },
   ]);
+  const handleAdd = (todo) => {
+    setTodos([...todos, todo]);
+  };
   return (
     <section>
       <ul>
@@ -12,6 +16,7 @@ export default function TodoList() {
           <li key={item.id}>{item.text}</li>
         ))}
       </ul>
+      <AddTodo onAdd={handleAdd} />
     </section>
   );
 }
